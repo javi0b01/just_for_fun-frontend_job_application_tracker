155e4398f1a25108f28b4a313cf55f2a15a46a3a
javi <55107141+javierandres-dev@users.noreply.github.com>
2024-12-29 18:22:45 -0500

    just for fun

--- src/app/components/pages/dashboard/dashboard.component.ts
+++ src/app/components/pages/dashboard/dashboard.component.ts
@@ -7,13 +7,36 @@ import {
   Validators,
 } from '@angular/forms';
 import { MessageService } from 'primeng/api';
-import { ButtonModule } from 'primeng/button';
+import { CalendarModule } from 'primeng/calendar';
+import { DropdownModule } from 'primeng/dropdown';
 import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { ButtonModule } from 'primeng/button';
+
+interface Response {
+  label: string;
+  value: string;
+}
+
+interface Model {
+  label: string;
+  value: string;
+}
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule, InputTextModule, ButtonModule],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    CalendarModule,
+    DropdownModule,
+    InputTextModule,
+    FloatLabelModule,
+    InputTextareaModule,
+    ButtonModule,
+  ],
   providers: [MessageService],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
@@ -21,19 +44,40 @@ import { InputTextModule } from 'primeng/inputtext';
 export class DashboardComponent {
   private messageServ = inject(MessageService);
 
+  responses: Response[] = [
+    { label: 'No Response', value: 'no response' },
+    { label: 'Interview', value: 'interview' },
+    { label: 'Offer Made', value: 'offer made' },
+    { label: 'Accepted', value: 'accepted' },
+    { label: 'Declined', value: 'declined' },
+    { label: 'Rejected', value: 'rejected' },
+  ];
+
+  models: Model[] = [
+    { label: 'Remote', value: 'remote' },
+    { label: 'Hybrid', value: 'hybrid' },
+    { label: 'On-Site', value: 'on-site' },
+  ];
+
   jatForm = new FormGroup({
-    dateApplied: new FormControl('', Validators.required),
-    response: new FormControl('', Validators.required), // declined, no response, interview, interested, rejected, accepted, offer made
-    position: new FormControl('', Validators.required),
-    model: new FormControl('', Validators.required), // remote, hybrid, on-site
-    source: new FormControl('', Validators.required), // careers page, linkedin,
-    company: new FormControl('', Validators.required),
-    contact: new FormControl('', Validators.required),
-    url: new FormControl(''),
-    uri: new FormControl(''),
-    email: new FormControl(''),
-    phone: new FormControl(''),
-    notes: new FormControl(''),
+    job: new FormGroup({
+      source: new FormControl<string | null>(null, Validators.required),
+      uri: new FormControl<string | null>(null),
+    }),
+    company: new FormGroup({
+      name: new FormControl<string | null>(null, Validators.required),
+      contact: new FormControl<string | null>(null),
+      url: new FormControl<string | null>(null),
+      email: new FormControl<string | null>(null),
+      phone: new FormControl<string | null>(null),
+    }),
+    application: new FormGroup({
+      dateApplied: new FormControl<Date | null>(null, Validators.required),
+      response: new FormControl<string | null>(null, Validators.required),
+      position: new FormControl<string | null>(null, Validators.required),
+      model: new FormControl<string | null>(null, Validators.required),
+      notes: new FormControl<string | null>(null),
+    }),
   });
 
   handleSubmit() {
